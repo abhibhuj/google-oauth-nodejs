60d7eb04e516a636c583c493aa80138b583da934
Abhishek Bhujaballi <abhishekbhujaballi@Abhisheks-Macbook-Pro.local>
2019-09-10 16:37:20 -0500

    removing client details

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ expressApp.get('/google/login', (req, res) => {
     const state = req.query.state;
     console.log('state------------------', state);
     const oauth2Client = new google.auth.OAuth2(
-        '412113996901-19bi8jsek70nnntc82g1l8uj6jn36srs.apps.googleusercontent.com',
-        'Tip4Y-BsKcuBb8nAnpMK773_', // e.g. _ASDFA%DFASDFASDFASD#FAD-
-        'https://oauth-redirect.googleusercontent.com/r/actions-codelab-e550e'
+        'client-id',
+        'client-secret',
+        'redirect-url'
     );
     const url = oauth2Client.generateAuthUrl({
         // 'online' (default) or 'offline' (gets refresh_token)
